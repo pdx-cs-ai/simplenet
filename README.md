635294d0435ebd9dafa51c69a17d89adc465d5af
Bart Massey <bart@cs.pdx.edu>
2019-11-18 15:43:20 -0800

    commented and documented

---
+++ README.md
@@ -0,0 +1,8 @@
+# simplenet: really small Neural Net demo
+Bart Massey
+
+This Python program tries to learn a two-input boolean
+function from random instances using either a single
+perceptron or a tiny neural net.
+
+Please see the code and comments for the details of operation.
--- sn.py
+++ sn.py
@@ -5,38 +5,49 @@
 
 import random
 
+# Learning rate.
 a = 0.01
 
+# Boolean xor.
 def lxor(x1, x2):
     return x1 ^ x2
 
+# Boolean and.
 def land(x1, x2):
     return x1 and x2
 
+# Function to learn.
 bfun = lxor
 
+# Generate n instances of the function f.
 def instances(n, f):
     def instance():
         xs = [random.randrange(2) for _ in range(2)]
         return [f(*xs), xs]
     return [instance() for _ in range(n)]
 
+# "Leaky ReLU" activation function. 
 def squash(h):
     if h >= 0:
         return h
     return 0.1 * h
 
+# Derivative of activation function.
 def dsquash(h):
     if h >= 0:
         return 1
     return 0.1
 
+# Single Perceptron.
 class Perceptron(object):
 
+    # Initialize with random positive weights and fixed
+    # bias.
     def __init__(self):
         self.ws = [0.1 + 0.9 * random.random() for _ in range(2)]
-        self.bias = 0
+        self.bias = 0.5
 
+    # Compute weighted sum of inputs xs and bias.
     def h(self, xs):
         t = 0
         for i in range(2):
@@ -44,11 +55,15 @@ class Perceptron(object):
         t += self.bias
         return t
 
+    # Compute output for input xs. Remember output
+    # for possible neural net learner.
     def y(self, xs):
         h = self.h(xs)
         self.yy = squash(h)
         return self.yy
 
+    # Train single perceptron with instance with features xs
+    # and class c.
     def train(self, c, xs):
         c0 = self.y(xs)
         e = c - c0
@@ -56,6 +71,10 @@ class Perceptron(object):
             self.ws[i] += a * xs[i] * e
         self.bias += a * e
 
+    # Train a net perceptron with input xs or predecessor
+    # layer preds. In the latter case, assume that y() has
+    # been called on each perceptron in preds to establish
+    # its output.
     def backprop(self, xs=None, preds=None):
         delta = a * self.ee * dsquash(self.yy)
         if preds != None:
@@ -69,18 +88,25 @@ class Perceptron(object):
                 self.ws[i] += preds[i].yy * delta
             self.bias += delta
 
+    # Print weights including bias.
     def print_ws(self):
         print(self.ws, self.bias)
 
+# Neural net consisting of a 2-perceptron
+# hidden layer and a one-perceptron output layer.
 class Net(object):
+
+    # Set up the net.
     def __init__(self):
         self.l1 = [Perceptron() for _ in range(2)]
         self.l2 = Perceptron()
     
+    # Compute the net output from its inputs.
     def y(self, xs):
         ys = [p.y(xs) for p in self.l1]
         return self.l2.y(ys)
         
+    # Train the net via backpropagation.
     def train(self, c, xs):
         y = self.y(xs)
         self.l2.ee = c - y
@@ -88,21 +114,29 @@ class Net(object):
         for p in self.l1:
             p.backprop(xs=xs)
 
+    # Print weights of all perceptrons in net.
     def print_ws(self):
         for p in self.l1:
             p.print_ws()
         self.l2.print_ws()
 
+# Learner to use for experiments.
+# net = Perceptron()
 net = Net()
 
+# Train the net on 1000 random instances
+# of the target function.
 train = instances(1000, bfun)
 for _ in range(1000):
     for c, xs in train:
         net.train(c, xs)
 
+# Show the weights of the resulting net.
 net.print_ws()
 print()
 
+# Show the learned output for each possible
+# input, both booleanized and in raw form.
 ttab = [[0, 0], [0, 1], [1, 0], [1, 1]]
 clas = [(bfun(*xs), xs) for xs in ttab]
 for c, xs in clas:
